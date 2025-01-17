bda570c704821d60c0697a389bf0e5d698172fdf
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2025-01-17 19:32:13 +0400

    new

---
+++ script.js
@@ -0,0 +1,12 @@
+const triangle = (b,h) =>{
+    var line = b*h;
+    var area = line / 2
+    return area;
+}
+
+export default triangle;
+
+const area = 5;
+const perimeter = 6;
+
+export {area, perimeter}
