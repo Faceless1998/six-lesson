0840645a2c1cb2c539369bdcf36df57f4c748f0b
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2025-01-20 20:02:47 +0400

    new

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
-const triangle = (b,h) =>{
-    var line = b*h;
-    var area = line / 2
-    return area;
+const prevButton = document.querySelector(".prev");
+const NextButton = document.querySelector(".next");
+const ImageContainer = document.querySelector(".carousel-images");
+
+let currentImage = 0;
+
+const updateCarousel = () => {
+    const width = window.innerWidth;
+    ImageContainer.style.transform = `translateX(-${currentImage * width}px)`
 }
 
-export default triangle;
+const goToNext = () => {
+    // currentImage = currentImage + 1;
+    // if(currentImage < ImageContainer.children.length-1){
+    //     currentImage = currentImage;
+    // }
+    // else if(currentImage === ImageContainer.children.length){
+    //     currentImage = 0;
+    // }
 
-const area = 5;
-const perimeter = 6;
 
-export {area, perimeter}
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    currentImage = (currentImage + 1) % ImageContainer.children.length;
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+    console.log(currentImage)
+    updateCarousel()
+}
+
+const goToPrev = () => {
+    // currentImage = currentImage - 1;
+    // if(currentImage >-1){
+    //     currentImage = currentImage;
+    // }
+    // else if(currentImage === -1){
+    //     currentImage = ImageContainer.children.length-1;
+    // }
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    currentImage = (currentImage - 1 + ImageContainer.children.length) % ImageContainer.children.length
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    console.log(currentImage)
+    updateCarousel()
+}
+
+prevButton.addEventListener("click",goToPrev)
+NextButton.addEventListener("click",goToNext)
+
+
+window.addEventListener("keydown", (event) => {
+    if(event.key === "ArrowRight"){
+        goToNext()
+    }else if(event.key === "ArrowLeft"){
+        goToPrev()
+    }
+})
