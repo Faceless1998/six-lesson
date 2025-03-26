27a942bd8f88250eedfa11c26a444cf93b34c54f
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2025-03-26 20:05:37 +0400

    new

--- navbar/src/App.js
+++ navbar/src/App.js
@@ -1,27 +1,92 @@
-import React from "react";
-import {Routes, Route} from "react-router-dom"
-
-import { Home } from "./pages/home/Home";
-import { About } from "./pages/about/About";
-import { Contact } from "./pages/contact/Contact";
-import { Registration } from "./pages/registration/Registration";
-
-import Layout from "./layout/Layout"
-import Footer from "./layout/Footer";
+import React, { useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [image, setImage] = useState(null);
+
+  const [editPostId, setEditPostId] = useState(null);
+  const [editTitle, setEditTitle] = useState("");
+  const [editContent, setEditContent] = useState("");
+  const [editImage, setEditImage] = useState(null);
+
+  const addPost = (title, content, image) => {
+    const newPost = {
+      id: Date.now(),
+      title,
+      content,
+      image,
+    };
+    setPosts([...posts, newPost]);
+  };
+
+  const editPost = (id, newTitle, newContent, newImage) => {
+    const updatedPost = posts.map((post) =>
+      post.id === id
+        ? { ...post, title: newTitle, content: newContent, image: newImage }
+        : post
+    );
+
+    setPosts(updatedPost);
+  };
+
+  const deletePost = (id) => {
+    const filteredPost = posts.filter((post) => post.id !== id);
+
+    setPosts(filteredPost);
+  };
+
+  const handleCreatePost = (e) => {
+    e.preventDefault();
+
+    if (title && content) {
+      addPost(title, content, image);
+      setTitle("");
+      setContent("");
+      setImage(null);
+    }
+  };
+
+  const handleEditPost = (e) => {
+    e.preventDefault();
+    if (editTitle && editContent) {
+      editPost(editTitle, editContent, editImage);
+      setEditTitle("");
+      setEditContent("");
+      setEditImage(null);
+    }
+  };
+
   return (
     <>
-      <Layout />
-      
-      <Routes>
-          <Route path="/" element={ <Home /> } />
-          <Route path="/about" element={ <About /> } />
-          <Route path="/contact" element={ <Contact /> } />
-          <Route path="/sign-in" element={ <Registration /> } />
-      </Routes>
+      <form onSubmit={handleCreatePost}>
+        <input
+          type="text"
+          placeholder="type..."
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          placeholder="description"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <input type="file" />
 
-      <Footer />
+        <input type="submit" value="submit" />
+      </form>
+
+      <div>
+        {posts.map((post) => (
+          <>
+            <div key={post.id}> {post.title} </div>
+            <div> {post.content} </div>
+            <div>{post.id}</div>
+          </>
+        ))}
+      </div>
     </>
   );
 }
