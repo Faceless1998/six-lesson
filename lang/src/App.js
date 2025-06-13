02616b29f2feb767ac43186d3e47bef280ad472a
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2025-06-13 19:21:56 +0400

    new

--- lang/src/App.js
+++ lang/src/App.js
@@ -1,21 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-import { useTranslation } from 'react-i18next';
+import "./App.css";
+import { useTranslation } from "react-i18next";
+import eng from "./asserts/Flag_of_England.svg.png";
+import frc from "./asserts/Flag_of_France.svg.webp";
+import geo from "./asserts/Flag_of_Georgia.svg.png";
+import { useState } from "react";
 
 function App() {
-  const {t, i18n} = useTranslation();
+  const { t, i18n } = useTranslation();
 
-  const changeLanguage = (lang) => {
-    i18n.changeLanguage(lang);
-  }
+  const [flag, setFlag] = useState("");
+
+  const siteLanguage = [
+    {
+      code: "en",
+      name: "English",
+      flag: eng,
+    },
+    {
+      code: "fr",
+      name: "French",
+      flag: frc,
+    },
+    {
+      code: "ka",
+      name: "Georgian",
+      flag: geo,
+    },
+  ];
+
+  const handleLanguageChange = (e) => {
+    i18n.changeLanguage(e.target.value);
+  };
+
+  const handleFlagChange = (e) => {
+    if (i18n.language === "en") {
+      setFlag(eng);
+    } else if (i18n.language === "ka") {
+      setFlag(geo);
+    } else {
+      setFlag(frc);
+    }
+  };
 
   return (
     <div className="App">
-      <h1> {t('welcome')} </h1>
-      <button onClick={() => changeLanguage('en')}>en</button>
-      <button onClick={() => changeLanguage('fr')}>fr</button>
-      <button onClick={() => changeLanguage('ka')}>ka</button>
+      <h1> {t("welcome")} </h1>
 
+      <div className="language-menu">
+        <div className="select-wrapper" >
+          <div className="flag" style={{backgroundImage: `${flag}`}}></div>
+          <div className="select-arrow globe">
+            <select
+              className="select"
+              onChange={handleLanguageChange}
+              value={i18n.language}
+            >
+              {siteLanguage.map((lang) => (
+                <option key={lang.code} value={lang.code}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
